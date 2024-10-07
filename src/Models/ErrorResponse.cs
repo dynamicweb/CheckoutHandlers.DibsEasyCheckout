6b80ab102ac3867a6fa50f776968a014a4d18091
Stanislav Smetanin <ssm@dynamicweb-cms.com>
2024-10-07 18:46:09 +1000

    Fixed problem with "Prefill customer address" setting and related data.

---
+++ src/Models/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Dynamicweb.Ecommerce.CheckoutHandlers.DibsEasyCheckout.Models;
+
+[DataContract]
+internal sealed class ErrorResponse
+{
+    [DataMember(Name = "errors")]
+    public Dictionary<string, IEnumerable<string>> Errors { get; set; }
+}
